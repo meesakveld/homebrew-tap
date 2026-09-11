97af1fffa720ff6e17408c8428b2060884c2f56f
meesakveld <meesakveld@gmail.com>
2026-09-11 14:00:16 +0200

    Fix context formula

--- Formula/context.rb
+++ Formula/context.rb
@@ -4,20 +4,24 @@ class Context < Formula
   version "1.0.0"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/meesakveld/context/releases/download/v1.0.0/context_darwin_arm64.tar.gz"
       sha256 "f5228998ebbddef803ae53f399cdf7945c7c45f31c4f5a3d28fc5a8f308e8295"
-    else
+    end
+
+    on_intel do
       url "https://github.com/meesakveld/context/releases/download/v1.0.0/context_darwin_amd64.tar.gz"
       sha256 "8c2f3c0dc47528131e384abefcfa47d9b2cfec9b0cfce3a4af605a5bd5012a81"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/meesakveld/context/releases/download/v1.0.0/context_linux_arm64.tar.gz"
       sha256 "9142d48cd57cb286ca51eae2706ade2a926b837912ca2b3baa26b5e65aea3060"
-    else
+    end
+
+    on_intel do
       url "https://github.com/meesakveld/context/releases/download/v1.0.0/context_linux_amd64.tar.gz"
       sha256 "f00b3ac706f3c519e158888c7cdfe33fb9fe2d2d6545de72424524f5131e4531"
     end
